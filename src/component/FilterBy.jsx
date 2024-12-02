e4ab0c215297082fb410deb4fec8f72d2dbd21a7
yahayagodiwa <yahayagodiwa8@gmail.com>
2024-12-02 13:04:03 +0100

    Filter by price added

--- src/component/FilterBy.jsx
+++ src/component/FilterBy.jsx
@@ -1,13 +1,13 @@
 import { Category } from "./Category"
 import { Color } from "./Color"
-// import { Price } from "./Price"
+import { Price } from "./Price"
 
 
 export const FilterBy = () => {
   return (
     <div className="fixed top-20">
     <Category  />
-    {/* <Price /> */}
+    <Price />
     <Color />
     </div>
   )
